a1d6bedc7820e4759ca99fae2ed00f0582a5a0aa
Seo ho Lee <shlee@exerchain.io>
2026-05-14 15:35:23 +0900

    for applying lanauge code

--- createus_common/choices.py
+++ createus_common/choices.py
@@ -1,4 +1,4 @@
-# filename: common/choices.py
+# filename: createus_common/choices.py
 
 from django.db import models
 
@@ -7,3 +7,26 @@ class LanguageChoice(models.IntegerChoices):
     KO = 1, "Korean"
     EN = 2, "English"
     ZH_HANT = 3, "Traditional Chinese"
+
+    @property
+    def code(self):
+        if self == LanguageChoice.KO:
+            return "ko"
+
+        if self == LanguageChoice.EN:
+            return "en"
+
+        if self == LanguageChoice.ZH_HANT:
+            return "zh-hant"
+
+        return "ko"
+
+    @classmethod
+    def to_code(cls, value):
+        if value is None:
+            return None
+
+        try:
+            return cls(value).code
+        except ValueError:
+            return None
